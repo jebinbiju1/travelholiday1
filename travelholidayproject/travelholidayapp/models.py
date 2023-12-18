78d253aaa5271ba47a58ac9cf5656afc05e4d85e
unknown <jebinbiju341@gmail.com>
2023-12-18 09:39:17 +0530

    added

---
+++ travelholidayproject/travelholidayapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Place(models.Model):
+    name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='picss')
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.name
